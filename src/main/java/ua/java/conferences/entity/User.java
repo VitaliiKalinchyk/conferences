a738108e8c9ccdb8f3f46b47db57fc9bcf9501f1
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-14 20:01:47 +0200

    added entity.User class

---
+++ src/main/java/ua/java/conferences/entity/User.java
@@ -0,0 +1,87 @@
+package ua.java.conferences.entity;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+    private boolean emailNotification;
+
+    public User() {}
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public boolean isEmailNotification() {
+        return this.emailNotification;
+    }
+
+    public void setEmailNotification(boolean emailNotification) {
+        this.emailNotification = emailNotification;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", emailNotification=" + emailNotification +
+                '}';
+    }
+}
