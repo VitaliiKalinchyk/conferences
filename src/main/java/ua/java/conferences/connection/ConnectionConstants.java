a2eb9e5e4597d9f8fff048a7355dc8d2852a299c
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-14 19:44:30 +0200

    added ConnectionConstants class

---
+++ src/main/java/ua/java/conferences/connection/ConnectionConstants.java
@@ -0,0 +1,23 @@
+package ua.java.conferences.connection;
+
+public final class ConnectionConstants {
+
+    private ConnectionConstants() {
+    }
+
+    public static final String SETTINGS_FILE = "connection.properties";
+
+    public static final String MYSQL = "MySql";
+
+    public static final String URL_PROPERTY = "connection.url";
+
+    public static final String USER_NAME = "user.name";
+
+    public static final String PASSWORD = "password";
+
+    public static final String CACHE_PREPARED_STATEMENT = "cachePrepStmts";
+
+    public static final String CACHE_SIZE = "prepStmtCacheSize";
+
+    public static final String CACHE_SQL_LIMIT = "prepStmtCacheSqlLimit";
+}
