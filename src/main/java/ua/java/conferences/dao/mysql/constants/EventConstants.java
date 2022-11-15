8476cae8e919265836392dbcbc71a08e30edec40
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-15 13:15:26 +0200

    updated EventConstants

--- src/main/java/ua/java/conferences/dao/mysql/constants/EventConstants.java
+++ src/main/java/ua/java/conferences/dao/mysql/constants/EventConstants.java
@@ -3,6 +3,8 @@ package ua.java.conferences.dao.mysql.constants;
 
 public final class EventConstants {
 
+    private EventConstants() {}
+
     public static final String ADD_EVENT = "INSERT INTO event (title, date, location, description) VALUES (?,?,?,?)";
 
     public static final String GET_EVENTS = "SELECT * FROM event";
@@ -23,6 +25,4 @@ public final class EventConstants {
 
     public static final String GET_REPORT_EVENT =
             "SELECT * FROM event JOIN report ON event.id=report.event_id WHERE report.id=?";
-
-    private EventConstants() {}
 }
