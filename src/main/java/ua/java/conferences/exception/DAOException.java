37a4eb7657a7c116ea342355ed633ef9b2f1214a
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-16 13:03:20 +0200

    renamed

---
+++ src/main/java/ua/java/conferences/exception/DAOException.java
@@ -0,0 +1,12 @@
+package ua.java.conferences.exception;
+
+public class DAOException extends Exception {
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
+}
