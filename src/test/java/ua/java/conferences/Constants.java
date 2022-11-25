dbe036db542af9f792578359f6e5f8d28e991a1a
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-25 10:00:11 +0200

    added some Tests Constants

---
+++ src/test/java/ua/java/conferences/Constants.java
@@ -0,0 +1,52 @@
+package ua.java.conferences;
+
+import java.time.LocalDate;
+
+public final class Constants {
+
+    public static final long ID = 1L;
+
+    public static final String EMAIL = "em@em.com";
+
+    public static final String WRONG_EMAIL = "em.com";
+
+    public static final String PASSWORD = "Password1";
+
+    public static final String WRONG_PASSWORD = "Pass1";
+
+    public static final String NAME = "Joe";
+
+    public static final String WRONG_NAME = "";
+
+    public static final String SURNAME = "Yi";
+
+    public static final String WRONG_SURNAME = "";
+
+    public static final String SPEAKER_NAME = "Joe Yi";
+
+    public static final boolean NOTIFICATION = false;
+
+    public static final String ROLE_NAME = "VISITOR";
+
+    public static final int ROLE_ID = 4;
+
+    public static final String TOPIC = "Report topic";
+
+    public static final String TITLE = "Event title";
+
+    public static final String DATE_NAME = "2022-11-25";
+
+    public static final LocalDate DATE = LocalDate.of(2022, 11, 25);
+
+    public static final String LOCATION = "Kyiv";
+
+    public static final String DESCRIPTION = "What an awesome event!";
+
+    public static final int REGISTRATIONS = 111;
+
+    public static final int VISITORS = 99;
+
+    public static final int REPORTS = 13;
+
+    private Constants() {}
+}
