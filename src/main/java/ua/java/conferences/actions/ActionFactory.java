ee00a7895185575ac26f5d8c0dfa0aa8aa22805e
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-27 09:21:17 +0200

    added actionfactory

--- src/main/java/ua/java/conferences/actions/ActionFactory.java
+++ src/main/java/ua/java/conferences/actions/ActionFactory.java
@@ -1,6 +1,6 @@
 package ua.java.conferences.actions;
 
-import ua.java.conferences.actions.implementation.*;
+import ua.java.conferences.actions.implementation.base.*;
 
 import java.util.*;
 
@@ -12,8 +12,8 @@ public final class ActionFactory {
 
     static {
         ACTION_MAP.put("sign-in", new SignInAction());
-        ACTION_MAP.put("sign-out", new SignOutAction());
         ACTION_MAP.put("sign-up", new SignUpAction());
+        ACTION_MAP.put("sign-out", new SignOutAction());
         ACTION_MAP.put("error", new ErrorAction());
     }
 
@@ -24,6 +24,6 @@ public final class ActionFactory {
     }
 
     public Action createAction(String actionName) {
-        return ACTION_MAP.getOrDefault(actionName, new  DefaultAction());
+        return ACTION_MAP.getOrDefault(actionName, new DefaultAction());
     }
 }
