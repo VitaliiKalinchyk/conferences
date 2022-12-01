f2fc0b2c0d5619475aeaf9768413bf4121ae4336
Vitalii Kalinchyk <karlkory@gmail.com>
2022-12-01 10:47:42 +0200

    minor changes

--- src/main/java/ua/java/conferences/exceptions/IncorrectEmailException.java
+++ src/main/java/ua/java/conferences/exceptions/IncorrectEmailException.java
@@ -3,6 +3,6 @@ package ua.java.conferences.exceptions;
 public class IncorrectEmailException extends ServiceException{
 
     public IncorrectEmailException() {
-        super("error.email.duplicate");
+        super("error.email.absent");
     }
 }
