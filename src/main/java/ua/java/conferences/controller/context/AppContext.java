5d6adfb86be4bb4d17b41978a905ae336d4def80
Vitalii Kalinchyk <karlkory@gmail.com>
2022-12-25 12:09:33 +0200

    added email sender

--- src/main/java/ua/java/conferences/controller/context/AppContext.java
+++ src/main/java/ua/java/conferences/controller/context/AppContext.java
@@ -2,6 +2,7 @@ package ua.java.conferences.controller.context;
 
 import lombok.*;
 import ua.java.conferences.model.services.*;
+import ua.java.conferences.utils.EmailSender;
 
 import static ua.java.conferences.model.dao.constants.DbImplementations.MYSQL;
 
@@ -10,6 +11,7 @@ public class AppContext {
     @Getter private final EventService eventService =  serviceFactory.getEventService();
     @Getter private final UserService userService = serviceFactory.getUserService();
     @Getter private final ReportService reportService = serviceFactory.getReportService();
+    @Getter private final EmailSender emailSender = new EmailSender();
 
     public static AppContext getAppContext() {
         return new AppContext();
