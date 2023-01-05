6ec41bd99d8a45b6ff0da7dfdd13a25023652f78
Vitalii Kalinchyk <karlkory@gmail.com>
2023-01-05 21:13:10 +0200

    fixed test

--- src/test/java/ua/java/conferences/controller/actions/implementation/base/SignOutActionTest.java
+++ src/test/java/ua/java/conferences/controller/actions/implementation/base/SignOutActionTest.java
@@ -1,17 +1,14 @@
 package ua.java.conferences.controller.actions.implementation.base;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.Assertions;
+import jakarta.servlet.http.*;
 import org.junit.jupiter.api.Test;
 import ua.java.conferences.controller.actions.util.MyRequest;
-import ua.java.conferences.controller.actions.util.MySession;
 import ua.java.conferences.dto.UserDTO;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static ua.java.conferences.controller.actions.constants.Pages.SIGN_IN_PAGE;
-import static ua.java.conferences.controller.actions.constants.Parameters.LOGGED_USER;
+import static ua.java.conferences.controller.actions.constants.Parameters.*;
 
 class SignOutActionTest {
     private final HttpServletRequest request = mock(HttpServletRequest.class);
@@ -19,9 +16,11 @@ class SignOutActionTest {
 
     @Test
     void testExecute() {
+        String en = "en";
         MyRequest myRequest = new MyRequest(request);
         myRequest.getSession().setAttribute(LOGGED_USER, UserDTO.builder().build());
+        myRequest.getSession().setAttribute(LOCALE, en);
         assertEquals(SIGN_IN_PAGE, new SignOutAction().execute(myRequest, response));
-        Assertions.assertFalse(((MySession)myRequest.getSession()).isValid);
+        assertEquals(en, myRequest.getSession().getAttribute(LOCALE));
     }
 }
