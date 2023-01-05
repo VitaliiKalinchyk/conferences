1c8401d134b3f495c2713a2efe85b660ee4e1829
Vitalii Kalinchyk <karlkory@gmail.com>
2023-01-05 17:24:29 +0200

    better SignOutAction.java

--- src/main/java/ua/java/conferences/controller/actions/implementation/base/SignOutAction.java
+++ src/main/java/ua/java/conferences/controller/actions/implementation/base/SignOutAction.java
@@ -5,14 +5,18 @@ import ua.java.conferences.controller.actions.Action;
 import jakarta.servlet.http.*;
 
 import static ua.java.conferences.controller.actions.constants.Pages.SIGN_IN_PAGE;
+import static ua.java.conferences.controller.actions.constants.Parameters.LOCALE;
 import static ua.java.conferences.controller.actions.constants.Parameters.LOGGED_USER;
 
 public class SignOutAction implements Action {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
-        if (request.getSession().getAttribute(LOGGED_USER) != null) {
-            request.getSession().invalidate();
+        HttpSession session = request.getSession();
+        if (session.getAttribute(LOGGED_USER) != null) {
+            String locale = (String) session.getAttribute(LOCALE);
+            session.invalidate();
+            request.getSession(true).setAttribute(LOCALE, locale);
         }
         return SIGN_IN_PAGE;
     }
