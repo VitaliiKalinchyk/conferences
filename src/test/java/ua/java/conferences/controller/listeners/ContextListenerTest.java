c7b94020de9a434bb953fa0b65c8bf841c2cc5b0
Vitalii Kalinchyk <karlkory@gmail.com>
2023-01-22 11:53:11 +0200

    100%

--- src/test/java/ua/java/conferences/controller/listeners/ContextListenerTest.java
+++ src/test/java/ua/java/conferences/controller/listeners/ContextListenerTest.java
@@ -2,7 +2,11 @@ package ua.java.conferences.controller.listeners;
 
 import jakarta.servlet.*;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import ua.java.conferences.controller.context.AppContext;
+
+import java.sql.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -16,5 +20,22 @@ class ContextListenerTest {
         when(sce.getServletContext()).thenReturn(servletContext);
         new ContextListener().contextInitialized(sce);
         assertNotNull(AppContext.getAppContext());
+    }
+
+    @Test
+    void testContextDestroyed() {
+        new ContextListener().contextDestroyed(sce);
+        assertEquals(0, DriverManager.drivers().count());
+    }
+
+    @Test
+    void testContextDestroyedException()  {
+        Driver driver = mock(Driver.class);
+        Stream<Driver> stream = Stream.of(driver);
+        try (MockedStatic<DriverManager> mocked = mockStatic(DriverManager.class)) {
+            mocked.when(DriverManager::drivers).thenReturn(stream);
+            mocked.when(() -> DriverManager.deregisterDriver(driver)).thenThrow(SQLException.class);
+            assertDoesNotThrow(() -> new ContextListener().contextDestroyed(sce));
+        }
     }
 }
