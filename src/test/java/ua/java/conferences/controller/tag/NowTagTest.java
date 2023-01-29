7c3c9f0af8c282528672f996bbcc57c756df6b10
Vitalii Kalinchyk <karlkory@gmail.com>
2023-01-29 14:54:06 +0200

    better test

--- src/test/java/ua/java/conferences/controller/tag/NowTagTest.java
+++ src/test/java/ua/java/conferences/controller/tag/NowTagTest.java
@@ -5,97 +5,22 @@ import jakarta.servlet.jsp.JspWriter;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.io.StringWriter;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class NowTagTest {
 
     @Test
     void testDoTag() throws IOException {
-        try(JspWriter jspWriter = new MyJspWriter()) {
+        try (JspWriter jspWriter = mock(JspWriter.class)) {
             JspContext jspContext = mock(JspContext.class);
             when(jspContext.getOut()).thenReturn(jspWriter);
             NowTag nowTag = new NowTag();
             nowTag.setJspContext(jspContext);
             assertDoesNotThrow(nowTag::doTag);
-            assertEquals(LocalDate.now().plusDays(1).toString(), jspWriter.toString());
+            verify(jspWriter).print(LocalDate.now().plusDays(1));
         }
-    }
-
-    private static class MyJspWriter extends JspWriter {
-
-        private final StringWriter stringWriter;
-
-        public MyJspWriter() {
-            super(4096, true);
-            stringWriter = new StringWriter();
-        }
-
-        @Override
-        public void print(Object o) {
-            stringWriter.write(o.toString());
-        }
-
-        @Override
-        public String toString() {
-            return stringWriter.toString();
-        }
-
-        @Override
-        public void newLine() {}
-        @Override
-        public void print(boolean b) {}
-        @Override
-        public void print(char c) {}
-        @Override
-        public void print(int i) {}
-        @Override
-        public void print(long l) {}
-        @Override
-        public void print(float v) {}
-        @Override
-        public void print(double v) {}
-        @Override
-        public void print(char[] chars) {}
-        @Override
-        public void print(String s) {}
-        @Override
-        public void println() {}
-        @Override
-        public void println(boolean b) {}
-        @Override
-        public void println(char c) {}
-        @Override
-        public void println(int i) {}
-        @Override
-        public void println(long l) {}
-        @Override
-        public void println(float v) {}
-        @Override
-        public void println(double v) {}
-        @Override
-        public void println(char[] chars) {}
-        @Override
-        public void println(String s) {}
-        @Override
-        public void println(Object o) {}
-        @Override
-        public void clear() {}
-        @Override
-        public void clearBuffer() {}
-        @Override
-        public void flush() {}
-        @Override
-        public void close() {}
-        @Override
-        public int getRemaining() {
-            return 0;
-        }
-        @Override
-        public void write(char[] charBuffer, int off, int len) {}
     }
 }
