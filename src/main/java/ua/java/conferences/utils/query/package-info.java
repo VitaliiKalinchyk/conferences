79d82a25725388497e29c09be120e0a7599da9ee
Vitalii Kalinchyk <karlkory@gmail.com>
2023-01-31 18:07:38 +0200

    updated javadoc

---
+++ src/main/java/ua/java/conferences/utils/query/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This is query package.
+ * It contains all necessary tools to build mysql queries
+ * QueryBuilder - abstract class, parent for all queryBuilders classes
+ * UserQueryBuilder - concrete class to build userDAO queries
+ * EventQueryBuilder - concrete class to build eventDAO queries
+ * VisitorEventQueryBuilder - concrete class to build eventDAO queries for visitor
+ *
+ * @author Vitalii Kalinchyk
+ * @version 1.0
+ */
+package ua.java.conferences.utils.query;
